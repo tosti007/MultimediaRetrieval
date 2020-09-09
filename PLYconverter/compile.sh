fc79d7ba5f3f51afad89b5c81c500d646d3d8bc4
Brian Janssen <brian-janssen@hotmail.com>
2020-09-09 17:42:48 +0200

    Add linux compile script

---
+++ PLYconverter/compile.sh
@@ -0,0 +1,1 @@
+gcc -o PlyConverter -lstdc++ -lm TriMesh_io.cc TriMesh_bounding.cc TriMesh_connectivity.cc TriMesh_curvature.cc  TriMesh_grid.cc TriMesh_normals.cc TriMesh_pointareas.cc TriMesh_stats.cc TriMesh_tstrips.cc ICP.cc KDtree.cc conn_comps.cc diffuse.cc edgeflip.cc faceflip.cc filter.cc make.cc merge.cc overlap.cc remove.cc reorder_verts.cc subdiv.cc umbrella.cc
