97980901e4ae107595fcc73de65e3ccdcd75c557
Brian Janssen <brian-janssen@hotmail.com>
2020-09-12 23:12:42 +0200

    Add changed boolean

--- Camera.cs
+++ Camera.cs
@@ -31,31 +31,57 @@ namespace MultimediaRetrieval
             return Matrix4.LookAt(Position, Position + Direction, _up);
         }
 
-        public void HandleInput(FrameEventArgs e, KeyboardState input)
+        public bool HandleInput(FrameEventArgs e, KeyboardState input)
         {
+            bool changed = false;
+
             if (input.IsKeyDown(Key.W))
+            {
                 Position += Direction * Speed * (float)e.Time; //Forward 
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.S))
+            {
                 Position -= Direction * Speed * (float)e.Time; //Backwards
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.A))
+            {
                 Position -= Vector3.Normalize(Vector3.Cross(Direction, _up)) * Speed * (float)e.Time; //Left
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.D))
+            {
                 Position += Vector3.Normalize(Vector3.Cross(Direction, _up)) * Speed * (float)e.Time; //Right
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.Space))
+            {
                 Position += _up * Speed * (float)e.Time; //Up 
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.LShift))
+            {
                 Position -= _up * Speed * (float)e.Time; //Down
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.E))
+            {
                 FOV += Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.Q))
+            {
                 FOV -= Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (FOV > 45.0f)
             {
@@ -67,10 +93,16 @@ namespace MultimediaRetrieval
             }
 
             if (input.IsKeyDown(Key.Left))
+            {
                 _yaw -= Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.Right))
+            {
                 _yaw += Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (_yaw > 360.0f)
             {
@@ -82,10 +114,16 @@ namespace MultimediaRetrieval
             }
 
             if (input.IsKeyDown(Key.Up))
+            {
                 _pitch += Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (input.IsKeyDown(Key.Down))
+            {
                 _pitch -= Sensitivity * (float)e.Time;
+                changed = true;
+            }
 
             if (_pitch > 89.0f)
             {
@@ -96,12 +134,17 @@ namespace MultimediaRetrieval
                 _pitch = -89.0f;
             }
 
-            Direction.X = (float)Math.Cos(MathHelper.DegreesToRadians(_pitch)) * (float)Math.Cos(MathHelper.DegreesToRadians(_yaw));
-            Direction.Y = (float)Math.Sin(MathHelper.DegreesToRadians(_pitch));
-            Direction.Z = (float)Math.Cos(MathHelper.DegreesToRadians(_pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(_yaw));
-            Direction = Vector3.Normalize(Direction);
-            _right = Vector3.Normalize(Vector3.Cross(Direction, Vector3.UnitY));
-            _up = Vector3.Normalize(Vector3.Cross(_right, Direction));
+            if (changed)
+            {
+                Direction.X = (float)Math.Cos(MathHelper.DegreesToRadians(_pitch)) * (float)Math.Cos(MathHelper.DegreesToRadians(_yaw));
+                Direction.Y = (float)Math.Sin(MathHelper.DegreesToRadians(_pitch));
+                Direction.Z = (float)Math.Cos(MathHelper.DegreesToRadians(_pitch)) * (float)Math.Sin(MathHelper.DegreesToRadians(_yaw));
+                Direction = Vector3.Normalize(Direction);
+                _right = Vector3.Normalize(Vector3.Cross(Direction, Vector3.UnitY));
+                _up = Vector3.Normalize(Vector3.Cross(_right, Direction));
+            }
+
+            return changed;
         }
     }
 }
