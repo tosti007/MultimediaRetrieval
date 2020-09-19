1340b2510edb05b7f61f08df597426098fcd41d7
Wink <33576429+desparito@users.noreply.github.com>
2020-09-19 19:14:40 +0200

    Update R script

--- R scripts/stats.R
+++ R scripts/stats.R
@@ -1,20 +1,22 @@
 data <- read.csv("./stats.csv", sep = ";", dec=",")
 
 #Histograms:
-hist(data$X.Faces, xlim=c(0,max(data$X.Faces)), breaks = 1000)  
-hist(data$X.Vertices, xlim=c(0,max(data$X.Vertices)), breaks = 1000)
+hist(data$X.Faces, xlim=c(0,max(data$X.Faces)), breaks = 1000, main="", xlab="Number of faces")  
+hist(data$X.Vertices, xlim=c(0,max(data$X.Vertices)), breaks = 1000, main="", xlab="Number of vertices")
 
 #Histograms for the lower end of the spectrum:
-hist(data$X.Faces, xlim=c(0,5000), breaks = 10000)  
-hist(data$X.Vertices, xlim=c(0,5000), breaks = 10000)
+hist(data$X.Faces, xlim=c(0,5000), breaks = 10000, main="", xlab="Number of faces")  
+hist(data$X.Vertices, xlim=c(0,5000), breaks = 10000, main="", xlab="Number of vertices")
 
 #Barplot for class value:
-barplot(height=table(data$Class), las=2)
+op <- par(mar = c(10,4,4,2) + 0.1)
+barplot(height=table(data$Class), las=2, main="")
+par(op)
 
 #Averages:
 avg_faces <- mean(data$X.Faces)
 avg_verts <- mean(data$X.Vertices)
-
+  
 avg_AABBminX <- mean(data$AABB.min.X)
 avg_AABBminY <- mean(data$AABB.min.Y)
 avg_AABBminZ <- mean(data$AABB.min.Z)
