aa3ae2ba7a1ba11b95bfbf6430811cc65d0e4c67
Brian Janssen <brian-janssen@hotmail.com>
2020-09-19 22:07:26 +0200

    Add autorun script for python

---
+++ MeshConverter/autorun.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/bash
+
+cd "$(dirname $0)"
+
+# Make pip use local environment instead of global install
+python -m venv env
+source env/bin/activate
+pip install -r requirements.pip
+
+# From step1 to step2
+mkdir -p ../database/step2
+python normalize.py
+
+# From step2 to step3
+mkdir -p ../database/step3
+python clean.py
+
+# From step3 to step4
+mkdir -p ../database/step4
+python refine.py
