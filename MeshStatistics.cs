f26f708b265d1cab2b4fd2053785bdc950ea2fcf
Brian Janssen <brian-janssen@hotmail.com>
2020-09-25 15:51:39 +0200

    Implement diameter and eccentricity

--- MeshStatistics.cs
+++ MeshStatistics.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using OpenTK;
+// For documentation check:
+// http://accord-framework.net/docs/html/R_Project_Accord_NET.htm
+// https://github.com/accord-net/framework/wiki
+using Accord.Math.Decompositions;
+using Accord.Statistics;
 
 namespace MultimediaRetrieval
 {
@@ -12,6 +17,9 @@ namespace MultimediaRetrieval
         public FaceType faceType;
         public AABB boundingBox;
         public float surface_area;
+        float diameter;
+        float eccentricity;
+        // TODO: compactness(with respect to a sphere)
 
         private MeshStatistics()
         {
@@ -35,6 +43,27 @@ namespace MultimediaRetrieval
             {
                 surface_area += f.CalculateArea(ref mesh.vertices);
             }
+
+            double[,] cov = new double[vertexCount,3];
+            this.diameter = float.NegativeInfinity;
+            for (int i = 0; i < mesh.vertices.Count; i++)
+            {
+                cov[i, 0] = mesh.vertices[i].position.X;
+                cov[i, 1] = mesh.vertices[i].position.Y;
+                cov[i, 2] = mesh.vertices[i].position.Z;
+
+                foreach (Vertex v in mesh.vertices)
+                {
+                    this.diameter = Math.Max(this.diameter, Vector3.DistanceSquared(mesh.vertices[i].position, v.position));
+                }
+            }
+            this.diameter = (float)Math.Sqrt(this.diameter);
+            cov = cov.Covariance(0);
+            // Eigenvectors are normalized, so retrieve from diagonalmatrix
+            double[,] eig = new EigenvalueDecomposition(cov).DiagonalMatrix;
+            float eig_max = (float)Math.Max(Math.Max(eig[0, 0], eig[1, 1]), eig[2, 2]);
+            float eig_min = (float)Math.Min(Math.Min(eig[0, 0], eig[1, 1]), eig[2, 2]);
+            this.eccentricity = eig_max / eig_min;
         }
 
         public const string Headers = 
@@ -50,7 +79,9 @@ namespace MultimediaRetrieval
             "AABB_max_Y;" + 
             "AABB_max_Z;" + 
             "AABB_Volume;" + 
-            "Surface_Area";
+            "Surface_Area" + 
+            "Diameter" +
+            "Eccentricity";
 
         public override string ToString()
         {
@@ -67,7 +98,9 @@ namespace MultimediaRetrieval
                 boundingBox.max.Y, 
                 boundingBox.max.Z, 
                 boundingBox.Volume(), 
-                surface_area
+                surface_area,
+                diameter,
+                eccentricity
                 );
         }
 
@@ -87,6 +120,8 @@ namespace MultimediaRetrieval
                 );
             // AABB Volume 
             stats.surface_area = float.Parse(data[12]);
+            stats.diameter = float.Parse(data[13]);
+            stats.eccentricity = float.Parse(data[14]);
 
             return stats;
         }
