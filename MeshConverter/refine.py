d2886fa38f218311d1eccbee24095385309015dd
Brian Janssen <brian-janssen@hotmail.com>
2020-10-03 14:49:12 +0200

    Rewrite refine.py so we do the loop in refine and coarse functions

--- MeshConverter/refine.py
+++ MeshConverter/refine.py
@@ -5,26 +5,41 @@ import trimesh as tm
 from meshparty import trimesh_vtk as tmvtk
 from meshparty.trimesh_io import Mesh
 
-def handle_mesh_refine(m):
-    v, f = tm.remesh.subdivide(m.vertices, m.faces)
-    return Mesh(v, f, process=False)
 
-def handle_mesh_coarse(m):
-    red = 1 - (1500/len(m.faces))
-    print("Reduction: ", red)
-    v, f = tmvtk.decimate_trimesh(m, reduction=red)
-    return Mesh(v, f, process=False)
+def mesh_refine(m, mid, min_vertices=None, min_faces=None):
+    v, f = (m.vertices, m.faces)
+    changed = False
+    
+    while (min_vertices is not None and len(v) < min_vertices) or (min_faces is not None and len(f) < min_faces):
+        print("Refine: ", mid)
+        changed = True
+        v, f = tm.remesh.subdivide(v, f)
+    
+    return (Mesh(v, f, process=False), changed)
+
+def mesh_coarse(m, mid, min_vertices=None, min_faces=None):
+    v, f = (m.vertices, m.faces)
+    changed = False
+    
+    while (min_vertices is not None and len(v) > max_vertices) or (min_faces is not None and len(f) > max_faces):
+        print("Coarse: ", mid)
+        red = 1 - ((max_vertices * 0.75)/len(m.faces))
+        print("Reduction: ", red)
+        changed = True
+        v, f = tmvtk.decimate_trimesh(m, reduction=red)
+    
+    return (Mesh(v, f, process=False), changed)
+
+def mesh_resize(opts, mid, m):
+    m, changed = mesh_refine(m, mid, 1000, 1000)
+
+    if not changed:
+        m, changed = mesh_coarse(m, mid, 2000, 2000)
+
+    return m
 
 def handle_mesh(opts, mid, m):
-    refined = False
-    while len(m.vertices) < 1000 or len(m.faces) < 1000:
-        refined = True
-        print("Refine: ", mid)
-        m = handle_mesh_refine(m)
-
-    if not refined and (len(m.vertices) > 2000 or len(m.faces) > 2000):
-        print("Coarse: ", mid)
-        m = handle_mesh_coarse(m)
+    m = mesh_resize(opts, mid, m)
     return m
 
 if __name__ == "__main__":
