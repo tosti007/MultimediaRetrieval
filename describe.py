bd83f11e35a5dd70d694bdf61051768146a7fab9
Brian Janssen <brian-janssen@hotmail.com>
2020-10-15 15:14:18 +0200

    Update python scripts

--- describe.py
+++ describe.py
@@ -6,4 +6,6 @@ import numpy as np
 
 df = pd.read_csv(sys.argv[1])
 df = df.groupby('Class').agg([np.mean, np.std])["Distance"]
-print(df.to_csv(sep=";"), end='')
+for c in df.columns:
+    print("Column:", c)
+    print(df[c].describe())
