c8002530051d7f93e0829d61caec6362f2720568
Brian Janssen <brian-janssen@hotmail.com>
2020-10-20 17:27:42 +0200

    Update plot_tsne

--- plot_tsne.py
+++ plot_tsne.py
@@ -1,19 +1,26 @@
 #! /usr/bin/python
 
+TOP5=True
+
 import sys
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 f = sys.argv[1] if len(sys.argv) > 1 else "database/output.mrtsne"
 
+
 df = pd.read_csv(f, sep=";")
 
-classes = df.groupby("Class").count()["X"].sort_values()[:-6:-1].index
+if TOP5:
+    classes = df.groupby("Class").count()["X"].sort_values(ascending=False).index
+    df = df[df["Class"].isin(classes[-5:])]
 
-sns.scatterplot(data=df, x="X", y="Y", hue="Class", legend=False)
-plt.savefig("plots/step4_tsne_all.jpg", dpi=300, transparent=True, bbox_inches=None)
+sns.scatterplot(data=df, x="X", y="Y", hue="Class", legend=TOP5, 
+        palette=sns.hls_palette(len(np.unique(df["Class"]))),
+        estimator=None, alpha=1 - (not TOP5) * 0.6)
 
-sns.scatterplot(data=df[df["Class"].isin(classes)], x="X", y="Y", hue="Class")
-plt.savefig("plots/step4_tsne_top5.jpg", dpi=300, transparent=True, bbox_inches=None)
+filename = "plots/step4_tsne_" + ("top5" if TOP5 else "all") + ".jpg"
+plt.savefig(filename, dpi=300, transparent=True, bbox_inches=None)
 
