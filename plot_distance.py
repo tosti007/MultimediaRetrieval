4e2a68e5aacc1cd75c5a4aadaf75cc80ca14b755
Brian Janssen <brian-janssen@hotmail.com>
2020-10-20 16:48:04 +0200

    Update plotting functions

--- plot_distance.py
+++ plot_distance.py
@@ -4,6 +4,7 @@ NR_SELECTED = 5
 SHOW_STD = False
 
 import sys
+import os
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -16,7 +17,7 @@ dfs = [f for f in sys.argv[1:]]
 
 for i, f in enumerate(dfs):
     dfs[i] = pd.read_csv(f)
-    dfs[i]["File"] = f
+    dfs[i]["Method"] = os.path.splitext(os.path.basename(f))[0]
 dfs = [df.sort_values(by=["Distance"]) for df in dfs]
 
 
@@ -40,15 +41,16 @@ for i in range(len(dfs)):
 
 # Plot the data as a dist plot, with a subplot for each file
 colors = sns.color_palette("Set1", len(selected))
-plots = sns.displot(data=pd.concat(dfs), palette=colors, kind="kde", x="Distance", hue="Class", row="File")
+plots = sns.displot(data=pd.concat(dfs), palette=colors, kind="kde", x="Distance", hue="Class", row="Method", facet_kws={"sharex":False, "sharey":False, "legend_out":False}, height=3, aspect=3)
+plots.legend.loc = "upper right"
 
 # Draw lines of the means on each subplot
 for i, df in enumerate(means):
+    plots.axes[i, 0].set_xlim(left=0)
     lims = plots.axes[i, 0].viewLim
     plots.axes[i, 0].vlines(df["mean"], lims.y0, lims.y1, colors=colors)
     if SHOW_STD:
         plots.axes[i, 0].vlines(df["std_min"], lims.y0, lims.y1 * 0.75, colors=colors)
         plots.axes[i, 0].vlines(df["std_max"], lims.y0, lims.y1 * 0.75, colors=colors)
-plt.xlim(left=0, right=7.5)
-plt.show()
+plt.savefig("plots/step4_distances.jpg", dpi=300, bbox_inches="tight")
 
--- plot_tsne.py
+++ plot_tsne.py
@@ -9,7 +9,11 @@ f = sys.argv[1] if len(sys.argv) > 1 else "database/output.mrtsne"
 
 df = pd.read_csv(f, sep=";")
 
-sns.scatterplot(data=df, x="X", y="Y", hue="Class", legend=False)
+classes = df.groupby("Class").count()["X"].sort_values()[:-6:-1].index
 
-plt.show()
+sns.scatterplot(data=df, x="X", y="Y", hue="Class", legend=False)
+plt.savefig("plots/step4_tsne_all.jpg", dpi=300, transparent=True, bbox_inches=None)
+
+sns.scatterplot(data=df[df["Class"].isin(classes)], x="X", y="Y", hue="Class")
+plt.savefig("plots/step4_tsne_top5.jpg", dpi=300, transparent=True, bbox_inches=None)
 
