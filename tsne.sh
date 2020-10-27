d4e405f92eb80db654f7e421204cc84a9e974794
Brian Janssen <brian-janssen@hotmail.com>
2020-10-27 17:39:28 +0100

    Add tsne creation script

---
+++ tsne.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+outdir="plots/tsne"
+call="mono bin/Debug/MultimediaRetrieval.exe "
+
+theta="$(seq 0.1 0.2 0.8 | sed 's/,/\./g')"
+perplexity="$(seq 71 10 91)"
+
+mkdir -p "$outdir"
+
+echo "Using theta:"
+echo "$theta"
+
+echo "Using perplexity:"
+echo "$perplexity"
+
+for t in $theta; do
+	for p in $perplexity; do
+		echo "Handling: $t and $p"
+		$call normalize --tsne "$p,$t"
+		python plot_tsne.py f database/output.mrtsne "$outdir/tsne_$(echo $p)_$t.jpg"
+	done
+done
+
+exit 0
